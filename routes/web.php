35401ed2f4b34f7dcb4262b1e8c59da03f52d151
kenlim2002 <limzkee@gmail.com>
2023-07-27 15:59:42 +0800

    try

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,6 @@ Route::middleware('auth')->group(function(){
     Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
     Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
     Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
-
 });
 
 
