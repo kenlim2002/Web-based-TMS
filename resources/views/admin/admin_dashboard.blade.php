0f9711a8aa8fda96cbf6f274dbcf1fa4edec20f9
kenlim2002 <limzkee@gmail.com>
2023-08-02 16:40:17 +0800

    delete hello world

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -5,7 +5,6 @@
       @include('include.sidebar')
       <div class="content ">
         <h1> Dashboard</h1>
-        <h1> hello world</h1>
         @include('include.chart')
         <div class="row">
           <div class="col-md-3">
